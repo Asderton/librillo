c385b3ffc162963b0427d882d29179467dad939e
Marcela Martinez <lmmartinez@fi.uba.ar>
2025-07-24 08:34:11 -0300

    levantar backend con docker compose

--- backend/modelos/funciones_db.js
+++ backend/modelos/funciones_db.js
@@ -1,12 +1,16 @@
-const { Pool } = require('pg');
-const db_client = new Pool({
-user: 'postgres',       
-host: 'localhost',      
-database: 'librillo',  
-password: 'postgres',   
-port: 5432,             
-});
 
+const { Pool } = require('pg');
+
+const db_client = new Pool({
+    host: 'librillo_db', 
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'librillo',
+    });
+
+db_client.connect()
+.then(() => console.log(" Conexión a PostgreSQL exitosa"))
+.catch(err => console.error(" Error al conectar a PostgreSQL:", err));
 
 module.exports = db_client;
-
