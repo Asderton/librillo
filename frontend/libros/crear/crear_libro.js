2a798e54e61183aac7e9a441c2fd34cdd90ce297
aeec <a.eec442@gmail.com>
2025-07-29 11:20:38 -0300

    Agregar autor a crear libro

--- frontend/libros/crear/crear_libro.js
+++ frontend/libros/crear/crear_libro.js
@@ -1,10 +1,20 @@
 const url_idiomas = "http://localhost:3000/api/idiomas";
+const url_autores = "http://localhost:3000/api/autores";
 
-function crear_opcion(idioma){
+
+function crear_opcion_idioma(idioma){
     const {id_idioma, nombre_idioma} = idioma
     const opcion = document.createElement('option');
     opcion.value = id_idioma;
     opcion.innerText = nombre_idioma;
+    return opcion;
+}
+
+function crear_opcion_autor(autor){
+    const {id_autor, nombre_completo} = autor
+    const opcion = document.createElement('option');
+    opcion.value = id_autor;
+    opcion.innerText = nombre_completo;
     return opcion;
 }
 
@@ -71,17 +81,14 @@ async function manejar_submit(event){
     validar_datos(datos_form);
     const datos_estandatizados = estandarizar_datos(datos_form);
 
-
     const respuesta = await fetch(url_post,{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(datos_estandatizados)
     });
 
-
-    console.log(typeof respuesta);
     if (respuesta.ok) {
-        window.location.href = '../../libros/';
+        window.location.href = '/frontend/libros/';
         return;
     } else {
         const error = await respuesta.json();
@@ -91,10 +98,20 @@ async function manejar_submit(event){
 }
 
 
-async function llenar_dropdown(idiomas) {
+async function llenar_idiomas(idiomas) 
+{
     const dropdown = document.getElementById("idiomas");
     for (const idioma of idiomas){
-        const opcion = crear_opcion(idioma); 
+        const opcion = crear_opcion_idioma(idioma); 
+        dropdown.appendChild(opcion);
+    }
+    return;
+}
+
+function llenar_autores(autores){
+    const dropdown = document.getElementById("autores");
+    for (const autor of autores){
+        const opcion = crear_opcion_autor(autor); 
         dropdown.appendChild(opcion);
     }
     return;
@@ -102,9 +119,14 @@ async function llenar_dropdown(idiomas) {
 
 
 async function fetch_data() {
-    const response = await fetch(url_idiomas);
-    const idiomas = await response.json();
-    llenar_dropdown(idiomas);
+    const respuesta_idiomas = await fetch(url_idiomas);
+    const idiomas = await respuesta_idiomas.json();
+    const respuesta_autores = await fetch(url_autores);
+    const autores = await respuesta_autores.json();
+
+    llenar_idiomas(idiomas);
+    llenar_autores(autores);
+
 }
 
 window.addEventListener('load', fetch_data);
